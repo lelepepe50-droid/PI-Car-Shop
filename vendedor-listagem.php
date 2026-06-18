67839f5f7571823479d635f466d0bc5043059a41
Kaio <kaiomascarenhas241@gmail.com>
2026-06-17 22:25:18 -0300

    update

--- vendedor-listagem.php
+++ vendedor-listagem.php
@@ -5,7 +5,7 @@ include "inc-menu.php"; // Adicionado ponto e vírgula que faltava
 ?>
 
 <body class="d-flex flex-column min-vh-100 bg-secondary">
-    <main class="container">
+    <main class="container pt-3 mb-2">
         <h1>listagem de vendedor</h1>
         <div class="row">
          <div class="col">
