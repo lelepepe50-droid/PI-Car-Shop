1176c4e89f649ba383283892e3db970913b50415
leandropro12 <227642795+lelepepe50-droid@users.noreply.github.com>
2026-07-01 10:34:52 -0300

    Update inc conexao

--- inc-conexao.php
+++ inc-conexao.php
@@ -1,7 +1,6 @@
 <?php
 // Tenta estabelecer a conexão com o banco de dados
-$conexao = mysqli_connect("localhost", "root", "", "db_carros_projeto_integrador");
-
+$conexao = mysqli_connect("localhost","root","", "db_carros_projeto_integrador");
 // Verifica se a conexão falhou
 if (!$conexao) {
     die("<h3>Erro ao conectar ao banco de dados:</h3>" . mysqli_connect_error());
